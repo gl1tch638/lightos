eb923bae7782584db2e07cd9046444da4dc27e73
gl1tch638 <savrscum.mail.box@proton.me>
2025-07-05 06:04:57 -0400

    Update setup.sh

--- .lightos/setup.sh
+++ .lightos/setup.sh
@@ -1,37 +1,26 @@
 #!/usr/bin/env bash
 
+touch install.log
+echo updating repos... please wait
+pkg update
+echo installing nescecarry packages...
+pkg -y install gum openssh
+
 # add while true loop later
 echo "please accept the storage permission"
 termux-setup-storage
 
-mv ~/lightos/.lightos ~
-cd ~/.lightos
-mv .bashrc ~/.bashrc
-cp theme/fonts/CaskaydiaCoveNerdFontMono-Regular.ttf ~/.termux/font.ttf
-cp theme/colors/base16-3024-light.properties ~/.termux/colors.properties
+gum spin --show-error --title "initalizing proot directories" -- mv ~/lightos/.lightos ~ && cd ~/.lightos && mv .bashrc ~/.bashrc && cp theme/fonts/CaskaydiaCoveNerdFontMono-Regular.ttf ~/.termux/font.ttf && cp theme/colors/base16-3024-light.properties ~/.termux/colors.properties
 
-echo installing proot tools...
-pkg update
-yes | pkg install x11-repo
-yes | pkg install termux-x11-nightly
-yes | pkg install pulseaudio
-yes | pkg install proot-distro
-yes | pkg install openssh
+gum spin --show-error --title "installing proot tools..." -- pkg -y install x11-repo >> install.log && pkg -y install termux-x11-nightly >> install.log && pkg -y install pulseaudio >> install.log && pkg -y install proot-distro >> install.log
 
-# echo installing termux user repo...
-# yes | pkg install tur-repo
-# echo installing hardware acceleration packages...
-# yes | pkg install mesa-zink virglrenderer-mesa-zink vulkan-loader-android virglrenderer-android
+gum spin --show-error --title "installing termux user repo..." -- pkg -y install tur-repo >> install.log
+gum spin --show-error --title "installing hardware acceleration packages..." -- pkg -y install mesa-zink virglrenderer-mesa-zink vulkan-loader-android virglrenderer-android >> install.log
 
-echo installing archlinux proot...
-proot-distro install archlinux
+gum spin --show-error --title "installing archlinux proot..." -- proot-distro install archlinux >> install.log
 
-ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux prootdir
-mkdir /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root/termuxdir
-ln -s ~ /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root/termuxdir/to
+gum spin --show-error --title "creating symlinks..." -- ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux prootdir && mkdir /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root/termuxdir -- ln -s ~ /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root/termuxdir/to
 
-cp rootsetupbase.sh rootsetup.sh
-mv rootsetup.sh /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root
-echo "chmod +x rootsetup.sh && ./rootsetup.sh" >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root/.profile
+gum spin --show-error --title "initalizing root setup step..." -- cp rootsetupbase.sh rootsetup.sh && mv rootsetup.sh /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root && echo "chmod +x rootsetup.sh && ./rootsetup.sh" >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root/.profile
 
 proot-distro login archlinux
