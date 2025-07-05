053b4fb40a4a34c17061c8bcaf5e6ef157896a54
gl1tch638 <savrscum.mail.box@proton.me>
2025-07-05 07:17:11 -0400

    Update setup.sh

--- .lightos/setup.sh
+++ .lightos/setup.sh
@@ -7,14 +7,14 @@ echo installing nescecarry packages...
 pkg -y install gum openssh &> install.log
 
 while true; do
-  echo "please accept the storage permission"
-  termux-setup-storage
-  sleep 1
-  echo "please accept the storage permission"
-  if test -d ~/storage then
-    break
-  else
-    echo "storage denied, please try again"
+echo "please accept the storage permission"
+termux-setup-storage
+sleep 1
+echo "please accept the storage permission"
+if test -d ~/storage then
+break
+else
+echo "storage denied, please try again"
 done
 
 gum spin --show-error --title "initalizing proot directories" -- mv ~/lightos/.lightos ~ && cd ~/.lightos && mv .bashrc ~/.bashrc && cp theme/fonts/CaskaydiaCoveNerdFontMono-Regular.ttf ~/.termux/font.ttf && cp theme/colors/base16-3024-light.properties ~/.termux/colors.properties
